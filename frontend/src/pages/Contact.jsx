1abc5e9e6adfb60984e61606907e97c504032e0c
Aditi Priya <aditipriya460@gmail.com>
2025-07-16 14:04:30 +0530

    Created all the pages- Completed frontend

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetter from '../components/NewsLetter'
 
 const Contact = () => {
     return (
         <div>
-        
+            
+            <div className='text-center text-2xl pt-10 border-t'>
+                <Title text1={'CONTACT'} text2={'US'}/>
+            </div>
+
+            <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+                <img src={assets.contact_img} alt="" className='w-full md:max-w-[450px]'/>
+                <div className='flex flex-col justify-center items-start gap-6'>
+                    <p className='text-xl font-semibold text-gray-600'>Our Store</p>
+                    <p className='text-gray-500'>MB-57 Shaharanpur  <br /> Suite 350, Delhi, India</p>
+                    <p className='text-gray-500'>Tel: (+91) 555-098-3456 <br /> Email: admin@forever.com</p>
+                    <p className='font-semibold text-xl text-gray-600'>Careerrs at Forever</p>
+                    <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+                    <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+                </div>
+            </div>
+
+            <NewsLetter />
+
         </div>
     )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [ currState, setCurrState ] = useState('Sign Up');
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>
-        
-        </div>
+        <form onSubmit={onSubmitHandler} 
+        className='flx flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-400'>
+            <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+                <p className='prata-regular text-3xl'>{currState}</p>
+                <hr className='border-none bg-gray-800 w-8 h-[1.5px]'/>
+            </div>
+            {currState === 'Login' ? '' : <input type="text" className='w-full px-3 py-2 border border-gray-800 mt-3' placeholder='Name' required/>}
+            <input type="email" className='w-full px-3 py-2 border border-gray-800 mt-3' placeholder='Email' required/>
+            <input type="password" className='w-full px-3 py-2 border border-gray-800 mt-3' placeholder='Password' required/>
+            <div className='w-full flex justify-between text-sm mt-[8px]'>
+                <p className='cursor-pointer'>Forgot your password?</p>
+                {
+                    currState === 'Login' ? 
+                    <p onClick={() => setCurrState('Sign Up')} 
+                    className='cursor-pointer'>Create Account</p> : 
+                    <p onClick={() => setCurrState('Login')} 
+                    className='cursor-pointer'>Login Here</p>
+                }
+            </div>
+            <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+        </form>
     )
 }
 
